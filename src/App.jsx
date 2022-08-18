d30e0bc54acd062ea25a3b9d953770a8ea572414
swapnil-talpade <swapniltalpade41@gmail.com>
2022-08-18 14:57:46 +0530

    adds search and top buttons

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Inputs from "./components/Inputs";
+import TopButtons from "./components/TopButtons";
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl">
+      <TopButtons />
+      <Inputs />
+    </div>
+  );
+}
+
+export default App;
