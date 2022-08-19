0a220fb6e0cdbbf0134b65ad907810c0e8f79791
swapnil-talpade <swapniltalpade41@gmail.com>
2022-08-19 12:25:45 +0530

    adds temparture details and location

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import Inputs from "./components/Inputs";
+import TemperatureDetails from "./components/TemperatureDetails";
+import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl">
       <TopButtons />
       <Inputs />
+      <TimeAndLocation />
+      <TemperatureDetails />
     </div>
   );
 }
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">Friday 19 August</p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Mumbai, IN</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeAndLocation;
