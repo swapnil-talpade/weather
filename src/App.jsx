903e038763e9a198eda5428b7a9ab9ddc23d1f64
swapnil-talpade <swapniltalpade41@gmail.com>
2022-08-19 16:13:47 +0530

    adds dynamic background change on temperature

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,17 @@ function App() {
     // fetchWeather();
   }, [query, units]);
 
+  const changeBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+    return "from-yellow-700 to-orange-700";
+  };
+
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl">
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br ${changeBackground()} h-fit shadow-xl`}
+    >
       <TopButtons setQuery={setQuery} />
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
