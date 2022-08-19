1099b680ebced596172c4f27259642c63d851c4f
swapnil-talpade <swapniltalpade41@gmail.com>
2022-08-19 16:57:27 +0530

    changed api key, nice try exploiting it :p

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,14 +1,12 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "7bcbbe90cf63d5e2cac541bd7f1239a9";
-
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({
     ...searchParams,
-    appid: API_KEY,
+    appid: process.env.REACT_APP_API_KEY,
   });
   return fetch(url)
     .then((res) => res.json())
